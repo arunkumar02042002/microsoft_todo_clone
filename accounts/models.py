ca7c909d6fd83083be319d4dda7ba5627a24c38d
Arun Kumar <arun.kumar.2403gg@gmail.com>
2024-01-08 22:07:14 +0530

    Created custom model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    bio = models.CharField(max_length=255, blank=True)
+    profile_img = models.ImageField(upload_to='users/profile_images', default='blank-profile-picture.png')
+
+    def save(self, *args, **kwargs):
+        # If bio is not provided, set a default bio based on the first_name
+        if not self.bio and self.first_name:
+            self.bio = f"I'm {self.first_name}."
+        super().save(*args, **kwargs)
