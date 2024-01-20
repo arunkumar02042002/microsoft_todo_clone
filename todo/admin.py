d4d442c46edda1576680ef8e01dd59ad821eeaf1
Arun Kumar <arun.kumar.2403gg@gmail.com>
2024-01-20 11:08:04 +0530

    Registered models

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task, TaskTag, Tag, List
 
 # Register your models here.
+admin.site.register(Task)
+admin.site.register(TaskTag)
+admin.site.register(Tag)
+admin.site.register(List)
