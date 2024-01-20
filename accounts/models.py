f6da522f4f953653af39fab12fbaac01484fefa0
arunkumar02042002 <arun.kumar.2403gg@gmail.com>
2024-01-20 21:40:58 +0530

    Changed default image

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     bio = models.CharField(max_length=255, blank=True)
-    profile_img = models.ImageField(upload_to='users/profile_images', default='blank-profile-picture.png')
+    profile_img = models.ImageField(upload_to='users/profile_images', default='blank-profile-picture.webp')
 
     def save(self, *args, **kwargs):
         # If bio is not provided, set a default bio based on the username
