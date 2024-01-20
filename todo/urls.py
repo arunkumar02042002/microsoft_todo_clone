017cbc768803287370586bac49cb0ea744fed67c
Arun Kumar <arun.kumar.2403gg@gmail.com>
2024-01-20 11:10:39 +0530

    Completed Task View and My day view

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import TaskListView, MyDayView
 
 urlpatterns = [
-    path('', view=home, name='home'),
+    path('', view=TaskListView.as_view(), name='home'),
+    path('myday/', view=MyDayView.as_view(), name='myday')
 ]
